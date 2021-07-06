f5cfc7d6581c9bb64b60a5c093914bbbce5bfe5e
ceogit <ceopass.com@gmail.com>
2021-07-06 13:23:37 -0400

    logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     company: "Alnexxxxx",
     domain: "https://master.d12j3a0n2yxsj0.amplifyapp.com/",
     defaultTitle:
-      "Alnex: Digital Agency Providing ML, Python, Node, React.js Engineering.",
+      "Alnexxxx: Digital Agency Providing ML, Python, Node, React.js Engineering.",
     preamble: "Alternative Visions for the Future.",
     defaultDescription: "NEW APPROACHES TO SOLVING OLD PROBLEMS.",
     postamble: "Think we can help your project? We'd love to hear from you!",
